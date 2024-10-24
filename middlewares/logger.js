a8e18fa8088e093ee373936b99b96434529a1b22
jay-228 <jay.mistry2208@gmail.com>
2024-10-24 11:44:26 +0530

    done

---
+++ middlewares/logger.js
@@ -0,0 +1,7 @@
+function logger(req, res, next) {
+    const timestamp = new Date().toString();
+    console.log(`URL: ${req.url}, Method: ${req.method}, Timestamp: ${timestamp}`);
+    next();
+}
+
+module.exports = logger;
